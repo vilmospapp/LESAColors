91e89bf3ccc4c16d6b33aa559fbcb4547d6c80b6
Vilmos Papp <vilmos.papp@liferay.com>
2016-03-24 10:15:17 +0100

    Initial Commit

---
+++ LESA Colors.js
@@ -0,0 +1,70 @@
+// ==UserScript==
+// @name         LESA Colors
+// @namespace    https://www.liferay.com/web/*/support
+// @version      0.1
+// @require      http://code.jquery.com/jquery-1.12.2.min.js
+// @require      https://gist.githubusercontent.com/raw/2625891/waitForKeyElements.js
+// @description  try to take over the world!
+// @author       Vilmos Papp
+// @grant        none
+// @match        https://www.liferay.com/web/*/support*
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+/*************************************************************/
+/* Color definitions                                         */
+/*************************************************************/
+var publicColor="rgb(255, 240, 200)"
+var publicColorDark="rgb(255, 230, 160)"
+var privateColor="rgb(210, 255, 200)"
+var privateColorDark="rgb(170, 255, 150)"
+
+/*************************************************************/
+/* Coloring code                                             */
+/*************************************************************/
+
+waitForKeyElements ("#_2_WAR_osbportlet_editor0", actionFunction);
+
+function actionFunction (jNode) {
+     var span = $("#_2_WAR_osbportlet_public")
+     if (span.hasClass("selected")) {
+        recolor(publicColor, publicColorDark)
+     }  
+}
+
+$('#_2_WAR_osbportlet_public').click(function() {
+    recolor(publicColor, publicColorDark)
+});
+
+$('#_2_WAR_osbportlet_workers').click(function() {
+    recolor(privateColor, privateColorDark)
+});
+
+$('#_2_WAR_osbportlet_liferay').click(function() {
+    recolor(privateColor, privateColorDark)
+});
+
+$('#_2_WAR_osbportlet_all-comments').click(function() {
+    resetColors()
+});
+
+$('#_2_WAR_osbportlet_history').click(function() {
+    resetColors()
+});
+
+$('#_2_WAR_osbportlet_solutions').click(function() {
+    resetColors()
+});
+
+function recolor(color, darkColor) {
+    $("#actualContent").css("background-color", color)
+    $("#_2_WAR_osbportlet_ticketFilter").css("background-color", color)
+    $(".sidebar").css("background-color", darkColor)
+}
+
+function resetColors() {
+    $("#actualContent").css("background-color", "white")
+    $("#_2_WAR_osbportlet_ticketFilter").css("background-color", "white")
+    $(".sidebar").css("background-color", "white")
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# LESA Colors
+
+This simple script makes it possible to change the background color of LESA based on the selected discussion tab to avoid unintended comments on the public tab.
+
+The script can be added to [GreaseMonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) or it's equivalent in other browsers like [Tampermonkey](http://tampermonkey.net/) for Chrome
+
+You can download the script and change the colors under the Colors definitions of the script:
+
+```
+/*************************************************************/
+/* Color definitions                                         */
+/*************************************************************/
+var publicColor="rgb(255, 240, 200)"
+var publicColorDark="rgb(255, 230, 160)"
+var privateColor="rgb(210, 255, 200)"
+var privateColorDark="rgb(170, 255, 150)"
+```
+
+### Use this script at your own risk, I do not take any responsibilities if it cause any problem for you.
